f1fbbc6b7f438b637bf97321eab8087cb5a0dbba
Adrian Sikora <adrian.sikora@sovanta.com>
2024-02-01 08:55:38 +0100

    init commit

---
+++ app.js
@@ -0,0 +1,134 @@
+(async function () {
+  const resourcePath = "https://asikoransp.github.io/sac/demo/resources/";
+  let currentStep = 0;
+  // const mockedStep = 21;
+
+  const res = await fetch("./config.json");
+  let options = await res.json();
+
+  options = options.map((el, i) => ({
+    ...el,
+    imagePath: `${resourcePath}${i}.png`,
+  }));
+
+  // ------ DOM ELEMENTS ------
+
+  const nextSlideBtn = document.getElementById("nextSlideBtn");
+  const startTourBtn = document.getElementById("startTour");
+  const backBtn = document.getElementById("backBtn");
+
+  const image = document.getElementById("demoImage");
+  const overlap = document.getElementById("tourOverlap");
+
+  const tooltip = document.getElementById("imageTooltip");
+  const title = document.getElementById("imageTitle");
+
+  const fullScreenBtn = document.getElementById("fullScreen");
+  const demoConatiner = document.getElementById("demoConatiner");
+
+  // ------ DOM ADJUSTMENTS METHODS ------
+
+  const displayOverlap = (show) => {
+    overlap.style.display = show ? "block" : "none";
+  };
+
+  const displayNextSlideBtn = (show) => {
+    nextSlideBtn.style.display = show ? "block" : "none";
+  };
+
+  const showNextSlideBtn = (show) => {
+    nextSlideBtn.style.visibility = show ? "visible" : "hidden";
+  };
+
+  const showBackBtn = (show) => {
+    backBtn.style.visibility = show ? "visible" : "hidden";
+  };
+
+  // ------ UTILS ------
+
+  const isNotFullScreenMode = () => {
+    return (
+      !document.fullscreenElement &&
+      !document.mozFullScreenElement &&
+      !document.webkitFullscreenElement &&
+      !document.msFullscreenElement
+    );
+  };
+
+  const updateNextStepBtn = (properties) => {
+    nextSlideBtn.style.width = properties.width;
+    nextSlideBtn.style.height = properties.height;
+    nextSlideBtn.style.top = properties.top;
+    nextSlideBtn.style.left = properties.left;
+  };
+
+  const updateNextStep = () => {
+    // currentStep = mockedStep;
+    updateNextStepBtn(options[currentStep]);
+    image.src = options[currentStep].imagePath;
+    tooltip.innerText = options[currentStep].tooltip;
+    title.innerText = options[currentStep].title;
+  };
+
+  // ------ HANDLERS ------
+
+  const nextStepHandler = (reset) => {
+    if (reset) {
+      // displayOverlap(false);
+      // displayNextSlideBtn(true);
+
+      displayOverlap(true);
+      displayNextSlideBtn(false);
+      showBackBtn(false);
+
+      currentStep = 0;
+    } else {
+      showBackBtn(true);
+    }
+
+    updateNextStep();
+  };
+
+  // ------ EVENTS ------
+
+  backBtn.addEventListener("click", () => {
+    currentStep--;
+    nextStepHandler(currentStep < 0);
+  });
+
+  nextSlideBtn.addEventListener("click", () => {
+    currentStep++;
+
+    showNextSlideBtn(false);
+
+    nextStepHandler(currentStep >= options.length);
+
+    setTimeout(() => {
+      showNextSlideBtn(true);
+    }, 200);
+  });
+
+  startTourBtn.addEventListener("click", () => {
+    displayOverlap(false);
+    displayNextSlideBtn(true);
+    showBackBtn(true);
+  });
+
+  document.addEventListener("fullscreenchange", () => {
+    demoConatiner.classList.toggle("full-screen-mode");
+    fullScreenBtn.classList.toggle("expand-icon");
+    fullScreenBtn.classList.toggle("collapse-icon");
+  });
+
+  fullScreenBtn.addEventListener("click", () => {
+    if (isNotFullScreenMode()) {
+      demoConatiner.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+  });
+
+  // ------ INIT ------
+
+  nextStepHandler(true);
+})();
